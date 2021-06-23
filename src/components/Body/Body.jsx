291de8ad1fcfa8bdc407fcd85f9094bed1a20139
Iskander <iskander.aydynov@mail.ru>
2021-06-23 13:29:08 +0300

    Structured

---
+++ src/components/Body/Body.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+import styles from "./Body.module.css"
+
+const Body = () =>
+    <main className={styles.body}>
+        <div>body</div>
+    </main>
+
+export default Body
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+import styles from "./Header.module.css"
+
+const Header = () =>
+    <header className={styles.header}>
+        <div>Questionnaire</div>
+    </header>
+
+export default Header
